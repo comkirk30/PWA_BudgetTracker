5681cf6a19e210c03adcdc6a481b2c9890120702
Courtney <courtneymkirk@hotmail.com>
2022-05-03 17:43:40 -0400

    adding service worker code/cache

--- service-worker.js
+++ service-worker.js
@@ -7,4 +7,28 @@ const FILES_TO_CACHE = [
   "/.js/idb.js",
   "/.css/styles.css",
   "/.js/index.js",
+  "./manifest.json",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png",
 ];
+
+self.addEventListener("fetch", function (e) {
+  console.log("fetch request : " + e.request.url);
+  e.answerWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        console.log("respond with cache : " + e.request.url);
+        return request;
+      } else {
+        console.log("file not cached, fetching : " + e.request.url);
+        return fetch(e.request);
+      }
+    })
+  );
+});
