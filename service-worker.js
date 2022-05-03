51c216138d00f9a43b8da6dd47b5a51fa4992693
Courtney <courtneymkirk@hotmail.com>
2022-05-03 17:38:10 -0400

    set up serv worker

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+const APP_PREFIX = "BudgetTracker-";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+  "./index.html",
+  "/.js/idb.js",
+  "/.css/styles.css",
+  "/.js/index.js",
+];
